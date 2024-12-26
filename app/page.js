81b1f05ccd71896d49d54edb1dfbbb0e768a4515
Mattis Hallberg <skatemattis97@hotmail.com>
2024-12-26 12:00:47 +0100

    polish homepage

--- app/page.js
+++ app/page.js
@@ -1,24 +1,39 @@
 import ButtonLogin from "@/components/ButtonLogin"
+import FAQListItem from "@/components/FAQListitem"
 
 export default function Home() {
   // här kan jag sätta variabel namn om jag vill och exempelvis sätta in i props.
   const isLoggedIn = true
   const name = "Mattis"
 
+  const pricingFeaturesList = [
+    "Collect customer feedback",
+    "Unlimited boards",
+    "Admin dashboard",
+    "24/7 support",
+  ]
+
   return (
     <main>
+      {/* Header */}
       <section className="bg-base-200">
         <div className="max-w-3xl mx-auto flex justify-between items-center px-8 py-2">
           <div className="font-bold">CodeFastSaaS</div>
           <div className="space-x-4 max-md:hidden">
-            <a className="link link-hover">Pricing</a>
-            <a className="link link-hover">FAQ</a>
+            <a className="link link-hover" href="#pricing">
+              Pricing
+            </a>
+            <a className="link link-hover" href="#faq">
+              FAQ
+            </a>
           </div>
           <div>
             <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
           </div>
         </div>
       </section>
+
+      {/* Hero */}
       <section className="text-center py-32 px-8 max-w-3xl mx-auto">
         <h1 className="text-4xl font-extrabold mb-6">
           Collect customer feedback to build better products
@@ -28,6 +43,85 @@ export default function Home() {
           better products your customers will love.
         </div>
         <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
+      </section>
+
+      {/* Pricing */}
+      <section className="bg-base-200">
+        <div className="py-32 px-8 max-w-3xl mx-auto">
+          <p className="text-sm uppercase font-medium text-center text-primary mb-6">
+            Pricing
+          </p>
+          <h2 className="text-3xl lg:text-4xl font-extrabold mb-12 text-center">
+            A pricing that adapts to your needs
+          </h2>
+
+          <div className="p-8 bg-base-100 w-96 rounded-3xl mx-auto">
+            <div className="flex gap-2 items-baseline">
+              <div className="text-4xl font-black">$19</div>
+              <div className="uppercase text-sm font-medium opacity-60">
+                /month
+              </div>
+            </div>
+
+            <ul className="space-y-2">
+              {pricingFeaturesList.map((priceItem) => {
+                return (
+                  <li className="flex gap-2 items-center" key={priceItem}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 16 16"
+                      fill="currentColor"
+                      className="size-4 text-green-600"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    {priceItem}
+                  </li>
+                )
+              })}
+            </ul>
+
+            <ButtonLogin
+              isLoggedIn={isLoggedIn}
+              name={name}
+              extraStyle="w-full my-3"
+            />
+          </div>
+        </div>
+      </section>
+
+      {/* FAQ frequently asked questions */}
+      <section className="bg-base-200">
+        <div className="py-32 px-8 max-w-3xl mx-auto">
+          <p className="text-sm uppercase font-medium text-center text-primary mb-6">
+            FAQ
+          </p>
+          <h2 className="text-3xl lg:text-4xl font-extrabold mb-12 text-center">
+            Frequently Asked Questions
+          </h2>
+          <ul className="max-w-lg mx-auto">
+            {[
+              {
+                question: "What do I get exactly?",
+                answer: "Dunno",
+              },
+              {
+                question: "Can I get a refund?",
+                answer: "Nein",
+              },
+              {
+                question: "I have another question",
+                answer: "lorem ipseium",
+              },
+            ].map((qa) => (
+              <FAQListItem key={qa.question} qa={qa} />
+            ))}
+          </ul>
+        </div>
       </section>
     </main>
   )
--- components/ButtonLogin.js
+++ components/ButtonLogin.js
@@ -1,9 +1,12 @@
 import Link from "next/link" // this pre fetches the data and hence makes loading faster. its not an html tag and the browser will interpret this as an a tag html.
 
-const ButtonLogin = ({ isLoggedIn, name }) => {
+const ButtonLogin = ({ isLoggedIn, name, extraStyle }) => {
   if (isLoggedIn) {
     return (
-      <Link href="/dashboard" className="btn btn-primary">
+      <Link
+        href="/dashboard"
+        className={`btn btn-primary ${extraStyle ? extraStyle : ""}`}
+      >
         Welcome back {name}
       </Link>
     )
